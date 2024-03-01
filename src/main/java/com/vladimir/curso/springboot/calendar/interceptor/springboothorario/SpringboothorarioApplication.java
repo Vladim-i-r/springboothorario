653a488c56ff4cfb730b9d4e2ed9ca9d8d21faae
Vladimir Lopez <vladimir.luna96@hotmail.com>
2024-03-01 16:04:31 -0600

    first commit

---
+++ src/main/java/com/vladimir/curso/springboot/calendar/interceptor/springboothorario/SpringboothorarioApplication.java
@@ -0,0 +1,13 @@
+package com.vladimir.curso.springboot.calendar.interceptor.springboothorario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboothorarioApplication.class, args);
+	}
+
+}
